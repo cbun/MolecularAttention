ab4e9ef2ec7c205498205a9a41147b6597d16182
Austin Clyde <aclyde@uchicago.edu>
2020-01-11 06:08:22 -0800

    added more properties

--- models/imagemodel.py
+++ models/imagemodel.py
@@ -3,9 +3,11 @@ import torchvision.models as models
 
 class ImageModel(nn.Module):
 
-    def __init__(self, dropout_rate = 0.15, intermediate_rep=128,  nheads=1):
+    def __init__(self, dropout_rate = 0.15, intermediate_rep=128,  nheads=1, outs=1):
         super(ImageModel, self).__init__()
         self.return_attn = True
+        self.outs = outs
+        self.nheads = nheads
 
         resnet18 = models.resnet101(pretrained=True)
         resnet18 = nn.Sequential(*list(resnet18.children())[:-1])
@@ -21,10 +23,10 @@ class ImageModel(nn.Module):
         )
 
         self.prop_model = nn.Sequential(
-            nn.Linear(intermediate_rep, 64),
+            nn.Linear(intermediate_rep, intermediate_rep),
             nn.ReLU(),
 
-            nn.Linear(64, 1)
+            nn.Linear(intermediate_rep, self.outs)
         )
 
 
@@ -32,7 +34,7 @@ class ImageModel(nn.Module):
         image = self.resnet181(features)
         attention = self.attention(image)
         attention = nn.functional.softmax(attention.view(attention.shape[0], -1), dim=-1).view(attention.shape)
-        attention = attention.repeat([1, 64, 1, 1])
+        attention = attention.repeat([1, int(2048 / self.nheads), 1, 1])
 
         image = self.resnet182(image * attention)
         image = image.view(features.shape[0], -1)
