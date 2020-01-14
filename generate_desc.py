a0e2e773ca6b86d735277eefcd28b7287d6b8ff1
Austin Clyde <aclyde@uchicago.edu>
2020-01-14 08:59:12 -0600

    added more properties

--- generate_desc.py
+++ generate_desc.py
@@ -23,14 +23,14 @@ if __name__=='__main__':
     df = pd.read_csv(args.i, header=None)
     smiles = []
     with multiprocessing.Pool(args.n) as p:
-        gg = filter(lambda x: x is not None, p.imap_unordered(validate_smiles, list(df.iloc[:, 0])))
+        gg = filter(lambda x: x is not None, p.imap(validate_smiles, list(df.iloc[:, 0])))
         for g in tqdm(gg, desc='validate smiles'):
             smiles.append(g)
     del df
 
     calc = Calculator(descriptors, ignore_3D=True)
     mols = map(Chem.MolFromSmiles, smiles)
-    df = calc.pandas(mols, nproc=32)
+    df = calc.pandas(mols, nproc=args.n)
     df = np.array(df, dtype=np.float16)
     df = np.nan_to_num(df, posinf=0, neginf=0, nan=0)
     np.save(args.o, df)
