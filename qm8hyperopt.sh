53514514449e02c05cfde7eceeb6226762d292fd
Austin Clyde <aclyde@anl.gov>
2020-01-20 11:44:52 -0500

    added

--- qm8hyperopt.sh
+++ qm8hyperopt.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 # Begin LSF Directives
 #BSUB -P med106
-#BSUB -W 12:00
-#BSUB -nnodes 22 
-#BSUB -q killable
+#BSUB -W 2:00
+#BSUB -nnodes 11
 #BSUB -J QMhyperopt
 #BSUB -o QMhyperopt.%J
 #BSUB -e QMhyperopt.%J
@@ -16,4 +15,4 @@ source source_to_run_pytorch1.0-p3
 
 cd     /gpfs/alpine/med106/proj-shared/aclyde/MolecularAttention
 export TORCH_HOME=/gpfs/alpine/med106/proj-shared/aclyde/torch_cache/
-jsrun -n132 -g1 -a1 -c7 python qm8_summit_tune.py
+jsrun -n66 -g1 -a1 -c7 python qm8_summit_tune.py
