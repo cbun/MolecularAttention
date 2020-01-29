900bf4762d2d956cc185650b395b8d40216dac24
Austin Clyde <aclyde@uchicago.edu>
2020-01-29 06:49:33 -0600

    added more properties

--- paper_runs/hacceptor.sh
+++ paper_runs/hacceptor.sh
@@ -4,10 +4,10 @@ python train.py --rotate -pb --amp O2 -w 1 --nheads 1 -r 0 --dropout_rate 0.1 -p
 
 python train.py --rotate -pb --amp O2 -w 1 --nheads 1 -r 0 --dropout_rate 0.1 -p hacceptor --depth 2 --width 128 -r 0 \
  --epochs 15 -b 512 --lr 1e-4 -i moses/test.smi --precomputed_values moses/test_hacceptor.npy \
-  --precomputed_images moses/tets_images.npy -o saved_models/moses_hacceptor.pt --eval_train  > moses_hacceptor_performance.txt
+  --precomputed_images moses/test_images.npy -o saved_models/moses_hacceptor.pt --eval_train  > moses_hacceptor_performance.txt
 
 python train.py --rotate -pb --amp O2 -w 1 --nheads 1 -r 0 --dropout_rate 0.1 -p hacceptor --depth 2 --width 128 -r 0 \
-  --epochs 15 -b 512 --lr 1e-4 -i moses/test_scaffolds.smi --precomputed_values moses/test_scaffold_hacceptor.npy \
+  --epochs 15 -b 512 --lr 1e-4 -i moses/test_scaffold.smi --precomputed_values moses/test_scaffold_hacceptor.npy \
   --precomputed_images moses/test_scaffold_images.npy -o saved_models/moses_hacceptor.pt --eval_train  >> moses_hacceptor_performance.txt
 
 
@@ -17,10 +17,10 @@ python train.py --rotate -pb --amp O2 -w 1 --nheads 1 -r 0 --dropout_rate 0.1 -p
 
 python train.py --rotate -pb --amp O2 -w 1 --nheads 1 -r 0 --dropout_rate 0.1 -p hacceptor --depth 2 --width 128 -r 0 \
  --epochs 15 -b 512 --lr 1e-4 -i moses/test.smi --precomputed_values moses/test_hacceptor.npy \
-  --precomputed_images moses/tets_images.npy -o saved_models/moses_hacceptor_bw.pt --eval_train  --bw >> moses_hacceptor_performance.txt
+  --precomputed_images moses/test_images.npy -o saved_models/moses_hacceptor_bw.pt --eval_train  --bw >> moses_hacceptor_performance.txt
 
 python train.py --rotate -pb --amp O2 -w 1 --nheads 1 -r 0 --dropout_rate 0.1 -p hacceptor --depth 2 --width 128 -r 0 \
-  --epochs 15 -b 512 --lr 1e-4 -i moses/test_scaffolds.smi --precomputed_values moses/test_scaffold_hacceptor.npy \
+  --epochs 15 -b 512 --lr 1e-4 -i moses/test_scaffold.smi --precomputed_values moses/test_scaffold_hacceptor.npy \
   --precomputed_images moses/test_scaffold_images.npy -o saved_models/moses_hacceptor_bw.pt --eval_train  --bw >> moses_hacceptor_performance.txt
 
 
@@ -30,8 +30,8 @@ python train.py --rotate -pb --amp O2 -w 1 --nheads 1 -r 0 --dropout_rate 0.1 -p
 
 python train.py --rotate -pb --amp O2 -w 1 --nheads 1 -r 0 --dropout_rate 0.1 -p hacceptor --depth 2 --width 128 -r 0 \
  --epochs 15 -b 512 --lr 1e-4 -i moses/test.smi --precomputed_values moses/test_hacceptor.npy \
-  --precomputed_images moses/tets_images.npy -o saved_models/moses_hacceptor_nopt.pt --eval_train --no_pretrain  >> moses_hacceptor_performance.txt
+  --precomputed_images moses/test_images.npy -o saved_models/moses_hacceptor_nopt.pt --eval_train --no_pretrain  >> moses_hacceptor_performance.txt
 
 python train.py --rotate -pb --amp O2 -w 1 --nheads 1 -r 0 --dropout_rate 0.1 -p hacceptor --depth 2 --width 128 -r 0 \
-  --epochs 15 -b 512 --lr 1e-4 -i moses/test_scaffolds.smi --precomputed_values moses/test_scaffold_hacceptor.npy \
+  --epochs 15 -b 512 --lr 1e-4 -i moses/test_scaffold.smi --precomputed_values moses/test_scaffold_hacceptor.npy \
   --precomputed_images moses/test_scaffold_images.npy -o saved_models/moses_hacceptor_nopt.pt --eval_train  --no_pretrain >> moses_hacceptor_performance.txt
