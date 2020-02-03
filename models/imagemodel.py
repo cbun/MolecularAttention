10ebbc5c36713287f4ee1a73b672d0599e1c9332
Austin Clyde <aclyde@uchicago.edu>
2020-02-03 07:39:15 -0600

    added more properties

--- models/imagemodel.py
+++ models/imagemodel.py
@@ -9,6 +9,7 @@ class ImageModel(nn.Module):
         self.outs = outs
         self.nheads = nheads
 
+
         if model_path is None:
             resnet18 = models.resnet101(pretrained=pretrain)
         else:
@@ -17,11 +18,15 @@ class ImageModel(nn.Module):
                 resnet18.load_state_dict(torch.load(model_path))
 
         resnet18 = nn.Sequential(*list(resnet18.children())[:-1])
-        self.resnet181 = nn.Sequential(*list(resnet18.children())[:5])
-        self.resnet182 = nn.Sequential(*list(resnet18.children())[5:])
-        self.attention = nn.Sequential(
-            nn.Conv2d(256, nheads, kernel_size=5, padding=2, stride=1),
-        )
+
+        if self.nheads > 0:
+            self.resnet181 = nn.Sequential(*list(resnet18.children())[:5])
+            self.resnet182 = nn.Sequential(*list(resnet18.children())[5:])
+            self.attention = nn.Sequential(
+                nn.Conv2d(256, nheads, kernel_size=5, padding=2, stride=1),
+            )
+        else:
+            self.resnet181 = nn.Sequential(*list(resnet18.children()))
 
         self.model = nn.Sequential(
             nn.Linear(2048, intermediate_rep),
@@ -50,14 +55,18 @@ class ImageModel(nn.Module):
 
 
     def forward(self, features):
-        image = self.resnet181(features)
-        attention = self.attention(image)
-        attention = nn.functional.softmax(attention.view(attention.shape[0], self.nheads, -1), dim=-1).view(attention.shape)
-        attention = attention.repeat([1, int(256 / self.nheads), 1, 1])
-        image = self.resnet182(image * attention)
-        image = image.view(features.shape[0], -1)
+        if self.nheads > 0:
+            image = self.resnet181(features)
+            attention = self.attention(image)
+            attention = nn.functional.softmax(attention.view(attention.shape[0], self.nheads, -1), dim=-1).view(attention.shape)
+            attention = attention.repeat([1, int(256 / self.nheads), 1, 1])
+            image = self.resnet182(image * attention)
+            image = image.view(features.shape[0], -1)
 
-        if self.return_attn:
-            return self.prop_model(self.model(image)), attention
+            if self.return_attn:
+                return self.prop_model(self.model(image)), attention
+            else:
+                return self.prop_model(self.model(image))
         else:
+            image = self.resnet181(features).view(features.shape[0], -1)
             return self.prop_model(self.model(image))
