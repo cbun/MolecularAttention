5aa97198b93697bc5332cddecc311311029dd0a5
Austin Clyde <aclyde@uchicago.edu>
2020-02-03 06:49:52 -0600

    added more properties

--- paper_runs/desc.sh
+++ paper_runs/desc.sh
@@ -1,3 +1,3 @@
 python train.py -pb --rotate --mae -p all -b 128 --epochs 25 --precomputed_values moses/train_descriptors.npy \
-  --lr 1e-4 -w 1 -i moses/train.smi -o saved_models/moses_descriptors.pt --nheads 64 --dropout_rate 0.15 --amp O1 \
-  --precomputed_images moses/train_images.npy --width 512  --depth 5 -r 0 --imputer_pickle moses/moses_scalers.pkl
+  --lr 1e-4 -w 1 -i moses/train.smi -o saved_models/moses_descriptors.pt --nheads 128 --dropout_rate 0.15 --amp O1 \
+  --precomputed_images moses/train_images.npy --width 1024  --depth 3 -r 0 --imputer_pickle moses/moses_scalers.pkl
