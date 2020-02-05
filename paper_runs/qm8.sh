0ac3b794a95371441e54fcdf05034d3abd000922
Austin <aclyde@anl.gov>
2020-02-05 08:33:55 -0600

    updated paper runs

--- paper_runs/qm8.sh
+++ paper_runs/qm8.sh
@@ -1,3 +1,3 @@
-python train.py -pb --rotate --mae -t 16 -b 128 --epochs 25 --precomputed_values qm8/qm8_values.npy \
-  --lr 1e-4 -w 1 -i qm8/qm8.smi -o saved_models/qm8.pt --nheads 64 --dropout_rate 0.15 --amp O1 \
-  --precomputed_images qm8/qm8_images.npy --width 128  --depth 5 --cv 1 -r 0 --no_pretrain -p custom
+python train.py -pb --rotate --mae -t 16 -b 128 --epochs 50 --precomputed_values qm8/qm8_values.npy \
+  --lr 5e-5 -w 1 -i qm8/qm8.smi -o saved_models/qm8_4.pt --nheads 0 --dropout_rate 0.1 --amp O1 \
+  --precomputed_images qm8/qm8_images.npy --width 256  --depth 4 --cv 4 -r 0 -p custom
